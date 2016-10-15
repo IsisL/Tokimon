cbba03ad96fcc78372111060d2bcca9e23237b9e
isisl <isisl@sfu.ca>
2016-10-15 03:51:20 -0700

    edit works

--- app/controllers/tokimons_controller.rb
+++ app/controllers/tokimons_controller.rb
@@ -1,22 +1,36 @@
 class TokimonsController < ApplicationController
 	def show
+		@tokimon = Tokimon.find(params[:id])
 	end 
 	def index
+		@tokimons = Tokimon.all
 	end
 	def new
 		@tokimon = Tokimon.new
 	end
 	def create
 		@tokimon = Tokimon.new(tokimon_params)
-		@tokimon.save
-		redirect_to @tokimon
+		if @tokimon.save
+			redirect_to @tokimon, :notice => "Success Tokimon created!"
+		else
+			render "new"
+		end
 	end
 	def edit
 		@tokimon = Tokimon.find(params[:id])
 	end
+	def update 
+		@tokimon = Tokimon.find(params[:id])
+		if @tokimon.update_attributes(tokimon_params)
+			redirect_to @tokimon, :notice => "Update complete!"
+		else 
+			render "edit"
+		end
+	end
 	def destroy
 		@tokimon = Tokimon.find(params[:id])
 		@tokimon.destroy
+		redirect_to tokimons_path, :notice => "Tokimon has been transferred to Professor Maple"
 	end
 	private 
 	def tokimon_params
